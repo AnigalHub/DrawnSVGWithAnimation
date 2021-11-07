58fda8ede51fa4583a316c00d3a1c3d854e9576a
AnigalHub <anigal1997@gmail.com>
2021-11-07 23:41:38 +0300

    добавлено svg - на Ff

--- src/components/alphabet.vue
+++ src/components/alphabet.vue
@@ -1,37 +1,37 @@
 <template>
     <div>
-        <h3>Alphabet</h3>
+        <h1>Alphabet</h1>
         <b-row>
             <b-col><aligator/></b-col>
             <b-col><bee/></b-col>
             <b-col><cat/></b-col>
             <b-col><dog/></b-col>
-            <b-col><bee/></b-col>
-            <b-col><cat/></b-col>
+            <b-col><elefant/></b-col>
+            <b-col><frog/></b-col>
         </b-row>
         <b-row>
             <b-col><bee/></b-col>
+            <b-col><frog/></b-col>
             <b-col><dog/></b-col>
+            <b-col><elefant/></b-col>
             <b-col><aligator/></b-col>
-            <b-col><cat/></b-col>
-            <b-col><bee/></b-col>
             <b-col><cat/></b-col>
         </b-row>
         <b-row>
             <b-col><aligator/></b-col>
             <b-col><bee/></b-col>
-            <b-col><cat/></b-col>
+            <b-col><frog/></b-col>
             <b-col><dog/></b-col>
-            <b-col><bee/></b-col>
+            <b-col><elefant/></b-col>
             <b-col><cat/></b-col>
         </b-row>
         <b-row>
-            <b-col><bee/></b-col>
+            <b-col><elefant/></b-col>
             <b-col><dog/></b-col>
             <b-col><aligator/></b-col>
             <b-col><cat/></b-col>
             <b-col><bee/></b-col>
-            <b-col><cat/></b-col>
+            <b-col><frog/></b-col>
         </b-row>
         <b-row>
             <b-col><bee/></b-col>
@@ -49,18 +49,21 @@
     import Bee from "./animals_svg/bee";
     import Cat from "./animals_svg/cat";
     import Dog from "./animals_svg/dog";
+    import Elefant from "./animals_svg/elefant";
+    import Frog from "./animals_svg/frog";
     export default {
         name: "alphabet",
-        components: {Aligator, Bee, Cat, Dog}
+        components: {Frog, Aligator, Bee, Cat, Dog, Elefant}
     }
 </script>
 
 <style scoped>
-svg{
-    margin-left: -170px;
-    margin-bottom: -25px;
-}
-    h3{
+    svg{
+        margin-left: -170px;
+        margin-bottom: -25px;
+    }
+    h1{
         margin-bottom: 35px !important;
+        font-family: 'Zen Antique', serif;
     }
 </style>
