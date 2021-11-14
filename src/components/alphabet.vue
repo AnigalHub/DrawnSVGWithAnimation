a94c107938e8044489d66814fa1a299db619002b
AnigalHub <anigal1997@gmail.com>
2021-11-14 14:48:52 +0300

    добавлены животные до S

--- src/components/alphabet.vue
+++ src/components/alphabet.vue
@@ -10,33 +10,33 @@
             <b-col><frog/></b-col>
         </b-row>
         <b-row>
-            <b-col><bee/></b-col>
-            <b-col><frog/></b-col>
-            <b-col><dog/></b-col>
-            <b-col><elefant/></b-col>
-            <b-col><aligator/></b-col>
-            <b-col><cat/></b-col>
+            <b-col><giraffe/></b-col>
+            <b-col><hamster/></b-col>
+            <b-col><iguana/></b-col>
+            <b-col><jellyfish/></b-col>
+            <b-col><kangaroo/></b-col>
+            <b-col><lion/></b-col>
         </b-row>
         <b-row>
-            <b-col><aligator/></b-col>
-            <b-col><bee/></b-col>
-            <b-col><frog/></b-col>
-            <b-col><dog/></b-col>
-            <b-col><elefant/></b-col>
-            <b-col><cat/></b-col>
+            <b-col><mouse/></b-col>
+            <b-col><numbat/></b-col>
+            <b-col><octopus/></b-col>
+            <b-col><panda/></b-col>
+            <b-col><quetzal/></b-col>
+            <b-col><raccoon/></b-col>
         </b-row>
         <b-row>
-            <b-col><elefant/></b-col>
-            <b-col><dog/></b-col>
-            <b-col><aligator/></b-col>
-            <b-col><cat/></b-col>
-            <b-col><bee/></b-col>
-            <b-col><frog/></b-col>
+            <b-col><sheep/></b-col>
+            <b-col></b-col>
+            <b-col></b-col>
+            <b-col></b-col>
+            <b-col></b-col>
+            <b-col></b-col>
         </b-row>
         <b-row>
-            <b-col><bee/></b-col>
-            <b-col><dog/></b-col>
-            <b-col><aligator/></b-col>
+            <b-col></b-col>
+            <b-col></b-col>
+            <b-col></b-col>
             <b-col></b-col>
             <b-col></b-col>
             <b-col></b-col>
@@ -51,19 +51,40 @@
     import Dog from "./animals_svg/dog";
     import Elefant from "./animals_svg/elefant";
     import Frog from "./animals_svg/frog";
+    import Giraffe from "./animals_svg/giraffe";
+    import Hamster from "./animals_svg/hamster";
+    import Iguana from "./animals_svg/iguana";
+    import Jellyfish from "./animals_svg/jellyfish";
+    import Kangaroo from "./animals_svg/kangaroo";
+    import Mouse from "./animals_svg/mouse";
+    import Lion from "./animals_svg/lion";
+    import Numbat from "./animals_svg/numbat";
+    import Octopus from "./animals_svg/octopus";
+    import Panda from "./animals_svg/panda";
+    import Quetzal from "./animals_svg/quetzal";
+    import Raccoon from "./animals_svg/raccoon";
+    import Sheep from "./animals_svg/sheep";
     export default {
         name: "alphabet",
-        components: {Frog, Aligator, Bee, Cat, Dog, Elefant}
+        components: {
+            Sheep,
+            Raccoon,
+            Quetzal,
+            Panda,
+            Octopus,
+            Numbat,
+            Lion, Mouse, Kangaroo, Jellyfish, Iguana, Hamster, Giraffe, Frog, Aligator, Bee, Cat, Dog, Elefant}
     }
 </script>
 
 <style scoped>
     svg{
-        margin-left: -170px;
-        margin-bottom: -25px;
+        margin-left: -145px;
+        margin-bottom: 10px;
     }
     h1{
-        margin-bottom: 35px !important;
+        margin-top: -5px !important;
+        margin-bottom: 40px !important;
         font-family: 'Zen Antique', serif;
     }
 </style>
