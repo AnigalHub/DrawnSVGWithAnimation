576a98a315bf131d220439eba0f60dc71d7b4565
AnigalHub <anigal1997@gmail.com>
2021-11-25 11:53:25 +0300

    добавлен router

--- src/main.js
+++ src/main.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueRouter from 'vue-router'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
+import routes from './routes';
+
+const router = new VueRouter(
+    {
+      mode:'history',
+      routes,
+      scrollBehavior (to, from, savedPosition) {
+        if (savedPosition) {
+          return savedPosition
+        } else {
+          return { x: 0, y: 0 }
+        }
+      }
+    });
 
 new Vue({
+  router,
   render: function (h) { return h(App) },
 }).$mount('#app')
