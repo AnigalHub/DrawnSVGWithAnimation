adac178ca0c639dadf8f78faaf6c0574a5d90839
AnigalHub <anigal1997@gmail.com>
2021-11-29 17:49:30 +0300

    страница с главным меню

---
+++ src/components/mainMenu.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="mainMenu">
+        <h1>Главное меню</h1>
+        <router-link :to="{name: 'GameMenu'}" tag="button">Изучение алфавита в игровой форме</router-link>
+        <router-link :to="{name: 'ColorChange'}" tag="button">Svg с анимацией по кнопке</router-link>
+        <router-link :to="{name: 'LoopingAnimation'}" tag="button">Svg с цикличной анимацией</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "mainMenu"
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
