e4afcecff2953fe5875793e649299b5aa40a5928
AnigalHub <anigal1997@gmail.com>
2021-11-29 17:48:59 +0300

    страница с меню (кнопочки) по игре

---
+++ src/components/gameMenu.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="gameMenu">
+        <h1>Меню игры</h1>
+        <router-link :to="{name: 'Alphabet'}" tag="button">Алфавит (справочник)</router-link>
+        <router-link :to="{name: 'CollectCouple'}" tag="button">Собери пару</router-link>
+        <router-link :to="{name: 'WordConstructor'}" tag="button">Конструктор слов</router-link>
+        <router-link :to="{name: 'MainMenu'}" tag="button" class="back">Назад</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "gameMenu"
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
