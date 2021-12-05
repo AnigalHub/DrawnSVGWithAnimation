a340e5c22c8748c3f9ce31f36450e4c5f4d90c92
AnigalHub <anigal1997@gmail.com>
2021-12-05 13:26:27 +0300

    добавлен пункт меню - ссылка на карту России

--- src/components/mainMenu.vue
+++ src/components/mainMenu.vue
@@ -4,6 +4,7 @@
         <router-link :to="{name: 'GameMenu'}" tag="button">Изучение алфавита в игровой форме</router-link>
         <router-link :to="{name: 'ColorChange'}" tag="button">Svg с анимацией по кнопке</router-link>
         <router-link :to="{name: 'LoopingAnimation'}" tag="button">Svg с цикличной анимацией</router-link>
+        <router-link :to="{name: 'Territory'}" tag="button">Svg с анимацией при наведении</router-link>
     </div>
 </template>
 
