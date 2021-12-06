a20a09e3ba56c1ab9b1d2d31afbe8c0bd7c3c888
AnigalHub <anigal1997@gmail.com>
2021-12-06 21:29:17 +0300

    компонент с картой

---
+++ src/components/territory.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="territory">
+        <router-link :to="{name: 'MainMenu'}" tag="button" class="back">Назад</router-link>
+        <div>
+            <territory-russia/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import TerritoryRussia from "./svg/territoryRussia";
+    export default {
+        name: "territory",
+        components: {TerritoryRussia}
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
