afea6e1693e9fa96a0847c4c5d7dfb8d636a246e
AnigalHub <anigal1997@gmail.com>
2021-12-24 21:02:17 +0300

    изменены местами кнопки

--- src/components/mainMenu.vue
+++ src/components/mainMenu.vue
@@ -3,8 +3,8 @@
         <h1>Главное меню</h1>
         <router-link :to="{name: 'GameMenu'}" tag="button">Изучение алфавита в игровой форме</router-link>
         <router-link :to="{name: 'ColorChange'}" tag="button">Svg с анимацией по кнопке</router-link>
-        <router-link :to="{name: 'LoopingAnimation'}" tag="button">Svg с цикличной анимацией</router-link>
         <router-link :to="{name: 'Territory'}" tag="button">Svg с анимацией при наведении</router-link>
+        <router-link :to="{name: 'LoopingAnimation'}" tag="button">Svg с цикличной анимацией</router-link>
     </div>
 </template>
 
