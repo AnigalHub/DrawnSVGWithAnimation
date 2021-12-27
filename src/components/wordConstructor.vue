fd76f026bc10bf2cba0e87ead90758001ff3e497
AnigalHub <anigal1997@gmail.com>
2021-12-27 21:33:56 +0300

    добавлен рандом свг (животного)

--- src/components/wordConstructor.vue
+++ src/components/wordConstructor.vue
@@ -9,12 +9,12 @@
             <div class="letter" v-for="(n,index) in  randomLettersAnimal" :key="index"
                  draggable @dragstart="startDrag($event,n,index)">{{n}}</div>
         </div>
-        <div class="name_animal"  @dragover.prevent
-             @dragenter.prevent  @drop="onDrop($event, animalName)">
-            <div class="letter" v-for="(x,index) in animalName" :key="index"
+        <div class="name_animal" >
+            <div class="letter" v-for="(x,index) in animalName" :key="index" :id="index"
+                 @dragover.prevent @dragenter.prevent  @drop="onDrop($event, animalName)"
                  draggable @dragstart="startDrag($event,x,index)">{{x}}</div>
         </div>
-        <b-button @click="getRandomSvg()">Далее</b-button>
+        <b-button @click="newAnimal()">Далее</b-button>
     </div>
 </template>
 
@@ -32,29 +32,35 @@
             }
         },
         created() {
-            this.currentSvg = this.getRandomSvg()
-            this.randomLettersAnimal = this.randomletters(this.currentSvg.letters)
+          this.newAnimal()
         },
         methods:{
-            randomletters:function (array) {
+            newAnimal:function(){
+                this.currentSvg = this.getRandomSvg()
+                this.randomLettersAnimal = this.randomLetters(this.currentSvg.letters)
+                this.animalName = new Array(this.randomLettersAnimal.length)
+            },
+            randomLetters:function (array) {
               return  array.map(i=>[Math.random(), i]).sort().map(i=>i[1])
             },
             startDrag(evt,item,id){
-                console.log("startDrag")
+                console.log(evt)
                 evt.dataTransfer.dropEffect = 'move'
                 evt.dataTransfer.effectAllowed = 'move'
                 console.log(item)
                 evt.dataTransfer.setData('letter', JSON.stringify({item:item,id:id}))
             },
             onDrop(evt,list){
-                console.log("onDrop")
                 const data = evt.dataTransfer.getData('letter')
                 let parsedData = JSON.parse(data)
-                console.log(parsedData)
-                list.push(parsedData.item)
-                console.log(list)
-                let a =   this.randomLettersAnimal.splice(parsedData.id,1)
+                console.log(evt)
+                list[evt.toElement.id]=parsedData.item
+
+                /*удаление*/
+                let a = this.randomLettersAnimal.splice(parsedData.id,1)
                 console.log(a)
+
+                console.log()
             }
         }
     }
@@ -88,8 +94,17 @@
     .name_animal{
         margin-top: 20px !important;
         height: 60px;
-        background: rgba(255, 255, 255, 0.5);
-        
+        //background: rgba(255, 255, 255, 0.5);
+        .letter{
+            background: rgba(255, 255, 255, 0.5);
+            border: 1.8px solid #9a9797;
+            float: left;
+            margin: 0 15px;
+            font-size: 2.25rem;
+            text-align: center;
+            width: 60px;
+            height: 60px;
+        }
     }
 </style>
 <style  lang="scss">
