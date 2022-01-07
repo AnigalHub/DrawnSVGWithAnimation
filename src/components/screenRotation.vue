adc86d04c288f943e04a7115ebbf483760baa11b
AnigalHub <anigal1997@gmail.com>
2022-01-07 13:06:24 +0300

    добавлен компонент - на вывод информации о повороте экрана

---
+++ src/components/screenRotation.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="screenRotation">
+        <h1>Переверните экран</h1>
+        <img src="../../public/telephone.png" height="100%" width="82%"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "screenRotation"
+    }
+</script>
+
+<style scoped lang="scss">
+    #screenRotation{
+       img{
+           padding: 10% 0;
+           margin: auto;
+           display: block;
+       }
+    }
+</style>
