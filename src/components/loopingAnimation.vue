0744796aee73ea487eec0102fbe2d2f78ae2a38c
AnigalHub <anigal1997@gmail.com>
2022-01-10 23:28:32 +0300

    добавлен компонент screen-rotation,блок с контентом, изменены стили

--- src/components/loopingAnimation.vue
+++ src/components/loopingAnimation.vue
@@ -1,18 +1,22 @@
 <template>
     <div id="loopingAnimation">
         <router-link :to="{name: 'MainMenu'}" tag="button" class="back">Назад</router-link>
-        <h1>Цикличная анимация</h1>
-        <div>
-            <laptop/>
+        <div id="content">
+            <h1>Цикличная анимация</h1>
+            <div>
+                <laptop/>
+            </div>
         </div>
+        <screen-rotation/>
     </div>
 </template>
 
 <script>
     import Laptop from "./svg/laptop";
+    import ScreenRotation from "./screenRotation";
     export default {
         name: "loopingAnimation",
-        components: {Laptop}
+        components: {ScreenRotation, Laptop}
     }
 </script>
 
@@ -21,13 +25,16 @@
         margin-top: 28px;
         margin-left: -20px;
     }
-    @media screen and  (min-width: 992px)and  (max-width: 1200px) {
+    @media screen and  (min-width: 500px)and  (max-width: 768px) {
         svg{
-            margin-top: 10px;
-            margin-left: -10px;
+            margin-top: -20px;
+            margin-bottom: 20px;
         }
     }
-</style>
-<style  lang="scss">
-
+    @media screen and  (min-width: 768px)and  (max-width: 1200px) {
+        svg{margin-top: 10px;}
+    }
+    @media screen and  (min-width: 500px)and  (max-width: 1200px) {
+        svg{margin-left: -40px;}
+    }
 </style>
