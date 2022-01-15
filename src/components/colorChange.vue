d59ca51793d27e8b2c871ce88bec0278a0e97ce5
AnigalHub <anigal1997@gmail.com>
2022-01-15 21:44:49 +0300

    стили изменены под scss, добавлена адаптивность, добавлен блок div с id=content, компонет screen-rotation

--- src/components/colorChange.vue
+++ src/components/colorChange.vue
@@ -1,16 +1,19 @@
 <template>
     <div id="colorChange">
         <router-link :to="{name: 'MainMenu'}" tag="button" class="back">Назад</router-link>
-        <h1>Нажми на кнопку</h1>
-        <div class="flex-container">
-            <div v-for="(letter,index) in Svgs" :key="index" >
-                    <label  class="switch">
-                        <input type="checkbox" v-model="Svgs[index].checked" >
-                        <span class="slider round">{{Svgs[index].checked ? "Вкл" : "Выкл"}}</span>
-                    </label>
-                    <component :isTurnOn="Svgs[index].checked" :is="letter.svg" :id="letter.name"/>
+        <div id="content">
+            <h1>Нажми на кнопку</h1>
+            <div class="flex-container">
+                <div v-for="(letter,index) in Svgs" :key="index" >
+                        <label  class="switch">
+                            <input type="checkbox" v-model="Svgs[index].checked" >
+                            <span class="slider round">{{Svgs[index].checked ? "Вкл" : "Выкл"}}</span>
+                        </label>
+                        <component :isTurnOn="Svgs[index].checked" :is="letter.svg" :id="letter.name"/>
+                </div>
             </div>
         </div>
+        <screen-rotation/>
     </div>
 </template>
 
@@ -21,9 +24,11 @@
     import Candle from "../components/colorChange_svg/candle";
     import Toaster from "../components/colorChange_svg/toaster";
     import Kettle from "../components/colorChange_svg/kettle";
+    import ScreenRotation from "./screenRotation";
     export default {
         name: "colorChange",
         components: {
+            ScreenRotation,
              Bulb,Alarm,Television,Candle,Toaster,Kettle
         },
         data(){
@@ -39,9 +44,12 @@
 
 <style scoped lang="scss">
     #colorChange{
-        .flex-container > div {
-            width: 33%;
-            margin-left: -40px;
+        .flex-container{
+            margin-left: 10%;
+            &> div {
+                width: 33%;
+                margin-left: -40px;
+            }
         }
     }
     .switch {
@@ -50,9 +58,7 @@
         width: 210px;
         height: 34px;
     }
-    input{
-        display: none;
-    }
+    input{display: none;}
     .slider {
         position: absolute;
         cursor: pointer;
@@ -65,16 +71,18 @@
         padding: 5px 0 0 105px;
         font-weight: bold;
         font-family: 'Alegreya', serif;
-    }
-    .slider:before {
-        position: absolute;
-        content: "";
-        height: 26px;
-        width: 26px;
-        left: 4px;
-        bottom: 4px;
-        background-color: white;
-        transition: .4s;
+
+        &:before {
+            position: absolute;
+            content: "";
+            height: 26px;
+            width: 26px;
+            top: 3px;
+            left: 4px;
+            bottom: 2px;
+            background-color: white;
+            transition: .4s;
+        }
     }
     input:checked + .slider {
         background-color: #2196F3;
@@ -83,6 +91,52 @@
     input:checked + .slider:before {transform: translateX(125px);}
     .slider.round {border-radius: 34px;}
     .slider.round:before {border-radius: 50%;}
-    svg{ margin-top: -40px; margin-bottom:45px;margin-left: -210px;}
+    svg{
+        width: 520px;
+        margin-top: 0;
+        margin-bottom:30px;
+        margin-left: -210px;
+    }
+    @media screen and (min-width: 500px) and (max-width: 768px) {
+        svg{width: 320px;}
+        #colorChange{
+            .flex-container{
+                margin-left: 30%;
+                &> div {
+                    width: 44%;
+                    margin-left: -40px;
+                    margin-right: 55px;
+                }
+            }
+        }
+        .slider {padding: 5px 45px 0 90px;}
+        .switch {
+            margin-left: -60%;
+            width: 170px;
+            height: 32px;
+        }
+        input:checked + .slider:before {transform: translateX(85px);}
+    }
+    @media screen and (min-width: 768px) and (max-width: 992px) {
+        svg{width: 380px;}
+        #colorChange{
+            .flex-container{
+                margin-left: 5%;
+                &> div {
+                    margin-right: 35px;
+                }
+            }
+        }
+        .slider {padding: 5px 45px 0 105px;}
+        .switch {
+            margin-left: -22%;
+            width: 190px;
+            height: 32px;
+        }
+        input:checked + .slider:before {transform: translateX(105px);}
+    }
+    @media screen and (min-width: 992px) and (max-width: 1200px) {
+        svg{width: 460px;}
+    }
 </style>
 
