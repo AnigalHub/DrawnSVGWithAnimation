e0e8fd9da4a45e7e62aea58a88d53e44b19beb2f
AnigalHub <anigal1997@gmail.com>
2022-01-15 21:46:53 +0300

    добавлен компонент ScreenRotation, блок вшм c id=content

--- src/components/territory.vue
+++ src/components/territory.vue
@@ -1,21 +1,31 @@
 <template>
     <div id="territory">
         <router-link :to="{name: 'MainMenu'}" tag="button" class="back">Назад</router-link>
-        <h1>Наведи на субъект на Карте</h1>
-        <div>
-            <territory-russia/>
+        <div id="content">
+            <h1>Наведи на субъект на Карте</h1>
+            <div>
+                <territory-russia/>
+            </div>
         </div>
+        <screen-rotation/>
     </div>
 </template>
 
 <script>
     import TerritoryRussia from "./svg/territoryRussia";
+    import ScreenRotation from "./screenRotation";
     export default {
         name: "territory",
-        components: {TerritoryRussia}
+        components: {ScreenRotation, TerritoryRussia}
     }
 </script>
 
 <style scoped lang="scss">
-
+    svg{
+        margin-left: -70px;
+        margin-top: -30px;
+    }
+    @media screen and (min-width: 500px) and (max-width: 768px) {
+        svg{margin-bottom: 20px;}
+    }
 </style>
