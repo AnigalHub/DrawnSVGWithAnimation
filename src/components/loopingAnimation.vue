022f85d344031782c76ad274986dd497a82cb758
AnigalHub <anigal1997@gmail.com>
2022-02-05 21:59:37 +0300

    удалено лишнее'

--- src/components/loopingAnimation.vue
+++ src/components/loopingAnimation.vue
@@ -21,20 +21,5 @@
 </script>
 
 <style scoped lang="scss">
-    svg{
-        margin-top: 28px;
-        margin-left: -20px;
-    }
-    @media screen and  (min-width: 500px)and  (max-width: 768px) {
-        svg{
-            margin-top: -20px;
-            margin-bottom: 20px;
-        }
-    }
-    @media screen and  (min-width: 768px)and  (max-width: 1200px) {
-        svg{margin-top: 10px;}
-    }
-    @media screen and  (min-width: 500px)and  (max-width: 1200px) {
-        svg{margin-left: -40px;}
-    }
+    svg{margin-top: -5px;}
 </style>
